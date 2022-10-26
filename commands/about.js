90e90e02be27e800e7647c30da729c87b2c6b56d
Mahdi Osman <mahdiosm2@gmail.com>
2022-10-26 13:58:40 +0300

    fixed errors

--- commands/about.js
+++ commands/about.js
@@ -9,13 +9,12 @@ module.exports = {
         // Format Embed
         const replyEmbed = new EmbedBuilder()
         .setColor(0xFA4454)
-        .setTitle(`${JSON.stringify(jsonData.data.name).replace(/"/g, '')}'s Account Details`)
+        .setTitle('ValorantStats Bot')
         .setThumbnail('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ViRuhE2KJirJx9yvzxAQP7oqN54LqfpVR1M249U&s')
         .addFields(
             { name: 'About', value: 'This was made as a personal project in order to test out Riot Games\' Valorant API' },
             { name: 'Disclaimer', value: 'We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with Riot Games.' },
-        )
-        .setImage(`${JSON.stringify(jsonData.data.card.wide).replace(/"/g, '')}`);
+        );
 
         // Bot Reply
         try {
